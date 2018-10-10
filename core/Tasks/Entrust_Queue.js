ad214f325ce1e6d10f4ead628810d75c9e9d2ef8
alfred_jc <funky@23>
2018-10-10 17:37:54 +0800

    check entrust id null or not

--- core/Tasks/Entrust_Queue.js
+++ core/Tasks/Entrust_Queue.js
@@ -86,6 +86,10 @@ async function getBuyEntrustList(coinExchangeId, refresh = true) {
 
 async function matchOrder(entrust) {
     try {
+        if (!entrust.entrust_id) {
+            return true
+        }
+        ;
         let result = await EntrustModel.updatEntrustCache(entrust);
         if (result.status == 0) {
             console.log("DB cannot find the entrust " + entrust.entrust_id);
